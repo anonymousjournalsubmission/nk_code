f53d7145535c15ca2c7939deaf9f03a64c4dd898
anonymousjournalsubmission <73268488+anonymousjournalsubmission@users.noreply.github.com>
2020-10-22 14:58:11 +1100

    Create __init__.py

---
+++ split_choson_munhak_pdfs/processors/__init__.py
@@ -0,0 +1,4 @@
+from .process_index import process_index
+from .process_text import process_text, process_multipage_text
+from .decode import decode_cid_strings
+from .pagination import process_pagination
